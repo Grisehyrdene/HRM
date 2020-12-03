6e90c8c0c9be4d8402b533d1af6166b19f5acc9a
Dmitri <Dmitri.Danshin2017@gmail.com>
2020-12-03 21:55:58 +0300

    tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-var slider = tns({
+const slider = tns({
     container: '.carousel__inner',
     items: 1,
     slideBy: 'page',
@@ -8,3 +8,35 @@ var slider = tns({
     ]
   });
 
+
+(function($) {
+    $(function() {
+      
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab-active)', function() {
+        $(this)
+          .addClass('catalog__tab-active').siblings().removeClass('catalog__tab-active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+      
+
+
+    });
+
+    
+
+    function toggleSlide(item){
+        $(item).each(function(i){
+            $(this).on('click', function(e){
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list')   .eq(i).toggleClass('catalog-item__list_active');
+    
+            });
+        });
+    };
+
+    toggleSlide('.catalog-item__back');
+    toggleSlide('.catalog-item__link');
+    
+
+})(jQuery);
