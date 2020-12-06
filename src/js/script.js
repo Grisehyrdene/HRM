bec465819bf37d195f71095aed761a2c3af51e6a
Dmitri <Dmitri.Danshin2017@gmail.com>
2020-12-06 17:02:44 +0300

    mailer

--- src/js/script.js
+++ src/js/script.js
@@ -79,4 +79,22 @@ const slider = tns({
   
   $('input[name=phone]').mask("+7 (999) 999-9999");
 
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+
+        $('form').trigger('reset');
+    });
+    return false;
+});
+
+
 })(jQuery);
